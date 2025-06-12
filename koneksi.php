138818f3005f823117589d97d2e88379b59c499c
sleepingindomie <82803431+sleepingindomie@users.noreply.github.com>
2025-06-12 19:26:16 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbs = "wsb";
+$servername = "wsb_generalday";
+$username = "wsb_generalday";
+$password = "92d05b0270339689fe78df0dfe81feb1db202e27";
+$dbs = "wsb_generalday";
 
 // Create connection
 $koneksi = mysqli_connect($servername, $username, $password, $dbs);
